12892a1685f2a3e7a89df05cc90833bb6e9d0748
SARAH-MIGADA-a <133857341+SARAH-MIGADA-a@users.noreply.github.com>
2023-07-09 17:09:46 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,7 @@
  */
 int _atoi(char *s)
 {
-       int i, d, n, len, f, digit;
+        int i, d, n, len, f, digit;
 
         i = 0;
         d = 0;
@@ -17,14 +17,14 @@ int _atoi(char *s)
         len = 0;
         f = 0;
         digit = 0;
-
+       
         while (s[len] != '\0')
                 len++;
 
         while (i < len && f == 0)
         {
-                if (s[i] == '-')
-                       ++d;
+               if (s[i] == '-')
+                        ++d;
 
                 if (s[i] >= '0' && s[i] <= '9')
                 {
@@ -39,8 +39,10 @@ int _atoi(char *s)
                 }
                 i++;
         }
+
         if (f == 0)
-               return (0);
+                return (0);
+
         return (n);
 }
 
