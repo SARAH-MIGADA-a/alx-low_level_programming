68b2e44a7bd7acb7c8a64ce9176ab67fb2b5711b
SARAH-MIGADA-a <133857341+SARAH-MIGADA-a@users.noreply.github.com>
2023-07-10 21:18:13 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,8 @@ char *str;
 unsigned int i;
 str = malloc(sizeof(char) * size);
 if (size == 0 || str == NULL)
-   return (NULL);
+return (NULL);
 for (i = 0; i < size; i++)
-    str[i] = c;
-    return (str);
+str[i] = c;
+return (str);
 }
