f05a7657c5187c74049baa7c4e20396f32ff2b00
SARAH-MIGADA-a <133857341+SARAH-MIGADA-a@users.noreply.github.com>
2023-07-11 16:35:40 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,11 +3,11 @@
 #include <ctype.h>
 #include <string.h>
 /**
- * check_num - check - string there are digit
- * @str: array str
- *
- * Return: Always 0 (Success)
- */
+* check_num - check - string there are digit
+* @str: array str
+*
+* Return: Always 0 (Success)
+*/
 int check_num(char *str)
 {
 /Declaring variables/
@@ -24,12 +24,12 @@ count++;
 return (1);
 }
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * Return: Always 0 (Success)
- */
+* main - Print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
+*
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
 /Declaring variables/
