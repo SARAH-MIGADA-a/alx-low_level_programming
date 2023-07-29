33b5ec9b437ef2734ad10273377cb440e2ff01c4
SARAH-MIGADA-a <133857341+SARAH-MIGADA-a@users.noreply.github.com>
2023-07-29 18:40:22 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - fibonacci <3
  *
@@ -7,48 +6,40 @@
  *
  * Return:  (Success)
  */
-
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
-
-	printf("%lu", bef);
-
-	for (i = 1; i < 91; i++)
-	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
-
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
-	}
-	printf("\n");
-	return (0);
+unsigned long int i;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
+printf("%lu", bef);
+for (i = 1; i < 91; i++)
+{
+printf(", %lu", aft);
+aft += bef;
+bef = aft - bef;
 }
-
-
+bef1 = (bef / l);
+bef2 = (bef % l);
+aft1 = (aft / l);
+aft2 = (aft % l);
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", aft1 + (aft2 / l));
+printf("%lu", aft2 % l);
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
+}
+printf("\n");
+return (0);
+}
 _putchar.c
-
 #include "main.h"
 #include <unistd.h>
 /**
@@ -60,15 +51,11 @@ _putchar.c
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
-
-
 main.h
-
 #ifndef main_h
 #define main_h
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -82,5 +69,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
 #endif
