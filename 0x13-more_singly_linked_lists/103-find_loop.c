d99cb62076b5d4628a2ba2cb45dc3b2d3f1d2d55
SARAH-MIGADA-a <133857341+SARAH-MIGADA-a@users.noreply.github.com>
2023-08-01 21:22:21 +0300

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * find_listint_loop - finds the loop in a linked list
  * @head: linked list to search for
@@ -7,24 +8,27 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *slow = head;
-listint_t *fast = head;
-if (!head)
-return (NULL);
-while (slow && fast && fast->next)
-{
-fast = fast->next->next;
-slow = slow->next;
-if (fast == slow)
-{
-slow = head;
-while (slow != fast)
-{
-slow = slow->next;
-fast = fast->next;
-}
-return (fast);
-}
-}
-return (NULL);
+	listint_t *slow = head;
+	listint_t *fast = head;
+
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
+		}
+	}
+
+	return (NULL);
 }
