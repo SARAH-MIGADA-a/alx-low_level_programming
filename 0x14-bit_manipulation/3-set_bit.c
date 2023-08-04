fc567a1a01fd77656eaa2643ec6520c9131a27d1
SARAH-MIGADA-a <133857341+SARAH-MIGADA-a@users.noreply.github.com>
2023-08-04 21:16:36 +0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * set_bit - sets a bit at a given index to 1
  * @n: pointer to the number to change
@@ -9,9 +8,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
-		return (-1);
-
-	*n = ((1UL << index) | *n);
-	return (1);
+if (index > 63)
+return (-1);
+*n = ((1UL << index) | *n);
+return (1);
 }
